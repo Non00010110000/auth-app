3fb0a23d94c2d9b2b5d0a853af7eb58e35854c60
Non00010110000 <nonhussien5@gmail.com>
2024-03-04 19:25:20 +0300

    create contoller and route folder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+app.use(express.json());
+
+app.use("/api/user", userRoute);
 
 app.listen(3000, () => {
   console.log("server on port 3000");
